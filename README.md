6e19f980fa1ab4ca63a684e52fcec7996ce30b06
Jordan Diez <jdiez316@gmail.com>
2018-09-14 09:18:46 -0400

    Couple minor tweaks, README update

--- README.md
+++ README.md
@@ -1,22 +1,17 @@
 # ddBz.io
 ### diezDesignBuildz
 
-Welcome to diezDezignBuildz, a platform built using _s and Wordpress. 
+Welcome to my humble digital abode. Currently building with _s and Wordpress. 
 
 Starting simple... a landing page, call to action, and going from there.
 
 **Next Steps:**
   * ~Analytics~
   * ~Mailto~ Contact Form / Postfix Config
-  * Better Logo
+  * ~Better Logo~
+  * ~Directory Listing~ / Flesh out content
   * Mobile Steez
-  * Directory Listing / Flesh out content
   * Testimonials/Referrals/Outreach
-  * Connecting the dots
-    * LinkedIn
-    * Flickr
-    * IRC
-    * github
-    * (design?)
+  * Connecting the dots, socially
   * Wordpress Proper
   * Publishing Platform
--- wp-content/themes/ddbz/front-page.php
+++ wp-content/themes/ddbz/front-page.php
@@ -27,7 +27,7 @@
 
     <p>But they don't make things like they used to, people change, and everything pixelated and new is exponentially more complex. Never before have we been more "connected", yet in so many ways we seem to be driven further apart. </p>
 
-    <p>DDBZ is how I do my part. DiezDezignBuildz is my process. I bring together my full stack of skills to map solutions and achieve goals, starting with my own. As I better develop the tools to serve myself, I'll be better able to serve you. </p>
+    <p>DDBZ is how I do my part. Diez Design Build is my process. I bring together my full stack of skills to map solutions and achieve goals, starting with my own. As I better develop the tools to serve myself, I'll be better able to serve you. </p>
 
     <p>I do: <span class="services">Digital Design/Build, Web Development, Graphic Design, Content Strategy, Copywriting, Marketing/SEO, and a bunch of other things...</span></p>
 
@@ -37,7 +37,7 @@
 
     <p>I'm passionate about leveraging technology for growth and empowerment. Do you know that the device that's currently connecting you to me is over a million times more powerful than the ones that sent man to the moon? That's nutz. This isn't an invitation to some wild insterstellar adventure... maybe someday... for now, let's talk bottle rockets.</p>
 
-    <p>Do you have something you'd like to launch? An idea? A business? The people who stand to benefit most from tech ar often the last served. Let's change that. I can help you design/build digital solutions that grow alongside you, your business, and your community. </p>
+    <p>Do you have something you'd like to launch? An idea? A business? The people who stand to benefit most from tech are often the last served. Let's change that. Let's design/build digital solutions that grow alongside you, your business, and your community. </p>
 
     <p>We've all got rockets in our pockets, but most don't know it. The pieces are all there, ready to be put together. </p>
 
@@ -46,7 +46,6 @@
   <div class="sidebar-right">
   <img class="hello jargon" src="<?php echo get_template_directory_uri() ?>/images/jargon.png">
   </div>
-  <footer><p>© 2018 diezDesignBuildz</p></footer>
-<?php wp_footer(); ?>
+<?php get_footer(); ?>
 </body>
 </html>
