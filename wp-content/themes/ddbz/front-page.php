b51c51f2a762751224c64c612b207784925626a0
Jordan Diez <jdiez316@gmail.com>
2019-02-01 13:34:13 -0500

    Updated written content. Minor styling.

--- wp-content/themes/ddbz/front-page.php
+++ wp-content/themes/ddbz/front-page.php
@@ -22,36 +22,46 @@
     <h2>helloWorld</h2>
     <h3>It's me... Jordan</h3>
 
-    <p>I love what I do.</p>
+    <p>I love solving problems. Putting the pieces together. When they come together just right... <span class="sprout">**BOOM!**</span> Becoming more than the sum of the parts.</p> 
 
-    <!-- Saving for later
+    <p>Do you know the device that's currently connecting you to me is over a million times more powerful than the ones that sent man to the moon? Let me help you make better use of it. Do you have something you'd like to launch? An idea? A business? <a href="#">Just pick up the phone.</a></p> 
+    
+    <p>I design and build digital solutions that make real life better. I do:</p>
+
+    <p class="services">Branding/Identity<br>Web Development<br>Content Strategy<br>Graphic Design<br>And more!</p>
+    <h3>Portfolio coming soon...</h3>
     <div class="tiles">
       <div class="tile"><h5>Balls</h5></div>
       <div class="tile"><h5>Cheese</h5></div>
       <div class="tile"><h5>Design</h5></div>
       <div class="tile"><h5>Diez</h5></div>
     </div>
-    -->
+    <h2>Design</h2>
 
-    <p>I love learning how things work. I find beauty in how all the assorted and occasionally complex pieces come together to function as one. How when they come together just right - BOOM - synergy - success - becoming more than the sum of the parts.</p>
+    <p>Design is what brings it, and us, all together. It solves big problems by merging knowledge and tools, ideas and people, the hardware and the software. It's about putting a deadline on a dream, and drawing our map from A to B.</p> 
 
-    <p>But they don't make things like they used to, people change, and everything pixelated and new is exponentially more complex. Never before have we been more "connected", yet in so many ways we seem to be driven further apart. </p>
+    <p>A is where you and I get to know each other. Grab coffee, talk shop. Get a sense of where your B might be. And why you're determined to get there. If you're anything like me, your map has all kinds of squiqqly lines with big red Xs and vast swaths of uncharted territory. The internet is a bit like the wild west. There's a lot to learn, and some things worth looking out for. Let me be your guide. I'll help you get from A to B, and maybe we can start charting a course to Z. I bring big picture thinking, a clear collaborative process, and an arsenal of skills that'll help us blow through any obstacles along the way.</p>
 
-    <p>DDBZ is how I do my part. Diez Design Build is my process. I bring together my full stack of skills to map solutions and achieve goals, starting with my own. As I better develop the tools to serve myself, I'll be better able to serve you. </p>
+    <p><a href="/contact">Giddy up.</a></p>
 
-    <p>I do: <span class="services">Digital Design/Build, Web Development, Graphic Design, Content Strategy, Copywriting, Marketing/SEO, and a bunch of other things...</span></p>
+    <h2>Diez</h2>
+    <h3>rhymes w/ beats</h3>
 
-    <p>I love what I do, but it could use more you. Want to collaborate?</p>
+    <p>It's my name. Don't worry, you won't wear it out, but say it right.</p> 
 
-    <h4><a href="#">... just pick up the phone ...</a></h4>
+    <p>A bit unusual, I know. But that's me. I'm a pretty unusual guy. And I bring an unusually diverse set of skills to my work.</p>
 
-    <p>I'm passionate about leveraging technology for growth and empowerment. Do you know that the device that's currently connecting you to me is over a million times more powerful than the ones that sent man to the moon? That's nutz. This isn't an invitation to some wild insterstellar adventure... maybe someday... for now, let's talk bottle rockets.</p>
+    <p>I've been smashing things apart and putting them back together since I was a kid. That's how I learn. We had this sweet little workshop in the basement growing up, decked out with all kinds of dangerous tools. I went from fixing toaster ovens to building (and crashing) bikes. If my ass wasn't bouncing on a bike seat, it was often firmly planted in front of a screen. My big bro and I grew up geeks and gamers.</p> 
+    
+    <p>I'm at home on the command line. I'm an adept wordsmith with a passion for pushing pixels. I've been a published photographer and a 3D modeler. In college I stretched my delicate wings with a year each in Spain and Denmark, grabbing an engineering degree along the way. I immediately landed what I considered a dream job working at a local bike startup. From there I moved to Vermont, grew a gnarly beard, and spent a few blissful years at an architectural design/build school, slinging huevos and hot meat sandwiches on the side. It was there that I developed a lens for the big picture, and came to see that I'm at my best when I bring all my seemingly disparate skills together.</p> 
 
-    <p>Do you have something you'd like to launch? An idea? A business? The people who stand to benefit most from tech are often the last served. Let's change that. Let's design/build digital solutions that grow alongside you, your business, and your community. </p>
+    <p>That we are at our collective best when we know how to come together, and collaborate. That gave me an idea. In 2010, my brain packed tight and percolating, I called up my best friend and asked him to build an education platform with me. It was an incredible ride. Tech has been my jam ever since.</p>
 
-    <p>We've all got rockets in our pockets, but most don't know it. The pieces are all there, ready to be put together. </p>
+    <p>I'm good at working with tight constraints and I place high value on time. When that time is up I unplug. I'm a new dad in a new old home in Western Mass. I like to play outside. I'm usually in bed by nine.</p> 
 
-    <h4><a href="#">let's play</a></h4>
+    <p>If all that's cool with you, what's say we <a href="/contact">chat about lighting that fuze?</a></p>
+
+    <h3>Stay tuned. Lots more to come.</h3>
   </section>
   <div class="sidebar-right">
     <img class="hello jargon" src="<?php echo get_template_directory_uri() ?>/images/jargon.png">
