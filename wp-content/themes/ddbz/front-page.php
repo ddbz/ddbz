06b224fcab3b4da7b9dbf05453626ee20eb4dce1
Jordan Diez <jdiez316@gmail.com>
2019-04-29 16:04:06 -0400

    Some good revisions on the text. New arrow setup/config. Still needs work.

--- wp-content/themes/ddbz/front-page.php
+++ wp-content/themes/ddbz/front-page.php
@@ -40,25 +40,37 @@
         <img class="slide-back" src="<?php echo get_template_directory_uri() ?>/images/brandIdentity.png" alt="Branding & Identity">
         <div id="arrow-left" class="arrow"></div>
         <div id="arrow-right" class="arrow"></div>
-        <p>We start at the heart of it. Clarity of purpose and vision is key to bring us into alignment. It's about asking the right questions and defining achievable goals. We establish a clear voice that captures the personality of your business and speaks to your ideal market. I run a digital whiteboard session to get this rolling. It's fun, interactive, and if you're local I can bring snacks.</p>
+        <div class="slideText">
+          <div class="arrowWrap"></div>
+          <p>We start at the heart of it. Clarity of purpose and vision is key to bring us into alignment. It's about asking the right questions and defining achievable goals. We establish a clear voice that captures the personality of your business and speaks to your ideal market. I run a digital whiteboard session to get this rolling. It's fun, interactive, and if you're local I can bring snacks.</p>
+        </div>
       </div>
       <div class="slide slide2">
         <img class="slide-back" src="<?php echo get_template_directory_uri() ?>/images/contentStrategy.png" alt="Branding & Identity">
         <div id="arrow-left" class="arrow"></div>
         <div id="arrow-right" class="arrow"></div>
-        <p>With a sweet logo and a well articulated brand in hand, we're off to the races. That core identity drives the design, charting our path to the prize. Content is king in this royal land. We owe it to the people. But beware, the internet is brimming with mythical beasts and wondrous feats. We do this right and we'll get you set up with a loyal steed and a rocket launcher.</p>
+        <div class="slideText">
+          <div class="arrowWrap"></div>
+          <p>With a sweet logo and a well articulated brand in hand, we're off to the races. That core identity drives the design, charting our path to the prize. Content is king in this royal land. We owe it to the people. But beware, the internet is brimming with mythical beasts and wondrous feats. We do this right and we'll get you set up with a loyal steed and a rocket launcher.</p>
+        </div>
       </div>
       <div class="slide slide3">
         <img class="slide-back" src="<?php echo get_template_directory_uri() ?>/images/webDesignDevelopment.png" alt="Branding & Identity">
         <div id="arrow-left" class="arrow"></div>
         <div id="arrow-right" class="arrow"></div>
-        <p>Devices are my vices. Sure, they're great for play or saying 'hey'. But looking beyond the glittery pixels, they are important tools with incredible potential to enhance us. To connect, amplify, and educate. Learning curves can be steep, but mastering their ways means putting a saddle on the beast of this digital world we live in. I get geeky with it so you don't have to. I'm a full stack designer/developer. If it's too much for just us, I'm happy to collab. I've got friends. When it comes to tech, I value simplicity. Whatever we build should look great and be easy for you and your customers to use.</p>
+        <div class="slideText">
+          <div class="arrowWrap"></div>
+          <p>Devices are my vices. Sure, they're great for play or saying 'hey'. But look beyond the glittery pixels and you'll see important tools with the potential to solve real problems. To connect, amplify, and educate. Learning curves can be steep, but mastering their ways means putting a saddle on the beast of this digital world we live in. I get geeky with it so you don't have to. I'm a full stack designer/developer. If it's too much for just us, I'm happy to collab. I've got friends. When it comes to tech, I value simplicity. Whatever we build should look great and be easy for you and your customers to use.</p>
+        </div>
       </div>
       <div class="slide slide4">
         <img class="slide-back" src="<?php echo get_template_directory_uri() ?>/images/brandIdentity.png" alt="Branding & Identity">
         <div id="arrow-left" class="arrow"></div>
         <div id="arrow-right" class="arrow"></div>
-        <p>Z is the boom I bring to all that I do. I seek synergy in diversity of people and ideas. When I find it, I bring consistency. In the quality of the work I do, and the creativity and passion with which I do it. Whether it's a cheeseburger or a website, I immerse myself in the craft. Let's get knocked off our feet. There's so much to learn in getting back up. When you get down to it, we're all riding the same rocket. It's clear we've got work to do. So I'll bring my boom, and you bring yours. Let's see how it flies.</p>
+        <div class="slideText">
+          <div class="arrowWrap"></div>
+          <p>Z is the boom I bring to all that I do. I seek synergy in diversity of people and ideas. When I find it, I bring consistency. In the quality of the work I do, and the creativity and passion with which I do it. Whether it's a cheeseburger or a website, I immerse myself in the craft. I'm not afraid to get knocked off my feet. What better way to figure out which direction is up. And there's so much to learn in getting back up. When you get down to it, we're all riding the same rocket. It's clear we've got work to do. So I'll bring my boom, and you bring yours. Let's see how it flies.</p>
+        </div>
       </div>
     </div>
     </div>
@@ -78,7 +90,7 @@
     <p>It's my name. Don't worry, you won't wear it out, but say it right.</p> 
     <p>A bit unusual, I know. But that's me. I'm a pretty unusual guy. And I bring an unusually diverse set of skills to my work.</p>
     <p>I've been smashing things apart and putting them back together since I was a kid. That's how I learn. We had this sweet little workshop in the basement growing up, decked out with all kinds of dangerous tools. I went from fixing toaster ovens to building (and crashing) bikes. If my ass wasn't bouncing on a bike seat, it was often firmly planted in front of a screen. My big bro and I grew up geeks and gamers.</p> 
-    <p>I'm at home on the command line. I'm an adept wordsmith with a passion for pushing pixels. I've been a published photographer and a 3D modeler. In college I stretched my delicate wings with a year each in Spain and Denmark, grabbing an engineering degree along the way. I immediately landed what I considered a dream job working at a local bike startup. From there I moved to Vermont, grew a gnarly beard, and spent a few blissful years at an architectural design/build school, slinging huevos and hot meat sandwiches on the side. It was there that I developed a lens for the big picture, and came to see that I'm at my best when I bring all my seemingly disparate skills together. That we are at our collective best when we know how to come together, and collaborate.</p> 
+    <p>I'm at home on the command line. I'm an adept wordsmith with a passion for pushing pixels. I've been a published photographer and a 3D modeler. In college I stretched my delicate wings with a year each in Spain and Denmark, grabbing an engineering degree along the way. I came home and promptly landed a potential dream job at a local bike startup. It wasn't. So I moved to Vermont, grew a gnarly beard, and spent a few blissful years at an architectural design/build school, slinging huevos and hot meat sandwiches on the side. It was there that I developed a lens for the big picture, and came to see that I'm at my best when I bring all my seemingly disparate skills together. That we are at our collective best when we know how to come together, and collaborate.</p> 
     <p>That gave me an idea. In 2010, my brain packed tight and percolating, I called up my best friend and asked him to build an education platform with me. It was an incredible ride. Tech has been my jam ever since.</p>
     <p>I'm good at working with tight constraints and I place high value on time. When that time is up I unplug. I'm a new dad in a new old home in Western Mass. I like to play outside. I'm usually in bed by nine.</p> 
     <p>If all that's cool with you, what's say we <a href="/contact">chat about lighting that fuze?</a></p>
