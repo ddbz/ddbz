41bd302517e65168a311934fd62560efe8ae46ec
Jordan Diez <jdiez316@gmail.com>
2020-04-03 15:41:38 -0400

    Fixed some style issues around pages & posts

--- wp-content/themes/ddbz/page-contact.php
+++ wp-content/themes/ddbz/page-contact.php
@@ -19,19 +19,20 @@ get_header();
     <img class="hello phone" src="<?php echo get_template_directory_uri() ?>/images/phone.png">
     </div>
     <section class="border-LR page-contact">
-		<?php
-		while ( have_posts() ) :
-			the_post();
+    <?php
+    while ( have_posts() ) :
+      the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+      get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+      // If comments are open or we have at least one comment, load up the comment template.
+      if ( comments_open() || get_comments_number() ) :
+        comments_template();
+      endif;
 
-		endwhile; // End of the loop.
-		?>
+    endwhile; // End of the loop.
+    ?>
+
     </section>
     <div class="sidebar-right">
     <img class="hello jargon" src="<?php echo get_template_directory_uri() ?>/images/jargon.png">
