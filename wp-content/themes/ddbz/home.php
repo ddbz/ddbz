3c69c488d3a57147287f38f0a2ad1e65a2f745d4
Jordan Diez <jdiez316@gmail.com>
2020-10-28 06:38:16 -0400

    Setup basic excerpts (bit janky) for the blog index

--- wp-content/themes/ddbz/home.php
+++ wp-content/themes/ddbz/home.php
@@ -1,6 +1,11 @@
 <?php
 /**
- * The template for displaying archive pages
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -13,16 +18,17 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php
+		if ( have_posts() ) :
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+			if ( is_home() && ! is_front_page() ) :
 				?>
-			</header><!-- .page-header -->
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
 
-			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
@@ -31,8 +37,9 @@ get_header();
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+         * ddBz - Modified below line and template file for excerpts. Not sure if it's the right way to do it.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/excerpt', get_post_type() );
 
 			endwhile;
 
@@ -49,5 +56,5 @@ get_header();
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+/* get_sidebar(); */
 get_footer();
